7066814dcced3ef5f57109431d844d4c7cd3f10e
foreverxxxxxx <emineogutlu3@gmail.com>
2024-08-11 18:20:43 +0300

    Data related to query by category id is displayed on the screen.

--- routes/user.js
+++ routes/user.js
@@ -3,8 +3,22 @@ const router = express.Router();
 
 const db = require("../data/db");
 
-router.use("/blogs/:blogid", function(req, res) {
-    res.render("users/blog-details");
+router.use("/blogs/:blogid", async function(req, res) {
+    const id = req.params.blogid;
+    try {
+        const [blog, ] = await db.execute("select * from blog where blogid=?", [id]);
+    if(blog[0]){
+
+        res.render("users/blog-details", {
+            title: blog[0].baslik,
+            blog: blog[0]
+        });
+}
+res.redirect("/");
+    }
+    catch(err) {
+        console.log(err);
+    }
 });
 
 router.use("/blogs", async function(req, res) {
