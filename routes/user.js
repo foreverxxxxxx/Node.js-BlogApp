d62d3d4d0cb585bfe88c738906047ad51c02c599
foreverxxxxxx <emineogutlu3@gmail.com>
2024-08-11 19:24:06 +0300

    After the category ids were queried from the database, they were sent to the relevant blogs ejs page.

--- routes/user.js
+++ routes/user.js
@@ -3,24 +3,37 @@ const router = express.Router();
 
 const db = require("../data/db");
 
+router.use("/blogs/category/:categoryid", async function(req, res) {
+    const id = req.params.categoryid;
+    try {
+        const [blogs, ] = await db.execute("select * from blog where categoryid=?", [id]); 
+        const [categories, ] = await db.execute("select * from category");
+
+        res.render("users/blogs", {
+            title: "Tüm Kurslar",
+            blogs: blogs,
+            categories: categories
+        })
+    }
+    catch(err) {
+        console.log(err);
+    }
+});
+
 router.use("/blogs/:blogid", async function(req, res) {
     const id = req.params.blogid;
     try {
         const [blogs, ] = await db.execute("select * from blog where blogid=?", [id]);
-        const blog=blogs[0];
 
+        const blog = blogs[0];
 
-    if(blogs){
-
-        res.render("users/blog-details", {
-            title: blogs.baslik,
-            blog: blogs
-        });
-}
-else{
-    res.redirect("/");
-}
-
+        if(blog) {
+            return res.render("users/blog-details", {
+                title: blog.baslik,
+                blog: blog
+            });
+        }
+        res.redirect("/");
     }
     catch(err) {
         console.log(err);
