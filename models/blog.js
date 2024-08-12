91c8c2aa0d44d212bb84af08c729fecfe719de54
foreverxxxxxx <emineogutlu3@gmail.com>
2024-08-12 13:12:04 +0300

    Category and Blog model created with sequelize

---
+++ models/blog.js
@@ -0,0 +1,45 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../data/db");
+
+const Blog = sequelize.define("blog", {
+    blogid: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    baslik: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    altbaslik: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    aciklama: {
+        type: DataTypes.TEXT,
+        allowNull: true
+    },
+    resim: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    anasayfa: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    },
+    onay: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    },
+    categoryid: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    eklenmeTarihi: {
+        type: DataTypes.DATETIME,
+        defaultValue: DataTypes.NOW
+    }
+});
+
+module.exports = Blog;
