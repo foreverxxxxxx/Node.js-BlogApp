a79a57116cd429b5bedc861125d149d1325c2b94
foreverxxxxxx <emineogutlu3@gmail.com>
2024-08-17 15:13:58 +0300

    A one-to-many relationship was established using hasMany and belongsTo.

--- index.js
+++ index.js
@@ -9,14 +9,34 @@ const path = require("path");
 const userRoutes = require("./routes/user");
 const adminRoutes = require("./routes/admin");
 
-const Blog = require("./models/blog");
-const Category = require("./models/category");
-
 app.use("/libs", express.static(path.join(__dirname, "node_modules")));
 app.use("/static", express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
 app.use(userRoutes); 
+
+const sequelize = require("./data/db");
+const dummyData = require("./data/dummy-data");
+const Category = require("./models/category");
+const Blog = require("./models/blog");
+
+Category.hasMany(Blog, {
+    foreignKey: {
+        name: 'categoryId',
+        allowNull: false,
+        // defaultValue: 1
+    }
+});
+Blog.belongsTo(Category);
+
+
+
+// IIFE
+(async () => {
+    await sequelize.sync({ force: true });
+    await dummyData();
+})();
+
 
 app.listen(3000, function() {
     console.log("listening on port 3000");
